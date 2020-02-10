56c5e93916fef920d416bcc88cf33bb76bcbf547
SCDan0624 <dmayle1@hotmail.com>
2020-02-10 14:08:12 -0800

    Added welcome message and half of get_character_movies_from_api method

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -1,11 +1,16 @@
 require 'rest-client'
 require 'json'
 require 'pry'
+require 'pp'
 
 def get_character_movies_from_api(character_name)
   #make the web request
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
+  character_data = response_hash["results"].find {|person| person["name"]==character_name}
+   
+  film_urls = character_data["films"]
+  
 
   # iterate over the response hash to find the collection of `films` for the given
   #   `character`
